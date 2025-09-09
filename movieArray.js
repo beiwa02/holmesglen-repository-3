a6714ed54f52d0f640f06d712c881d07ef5eb6ef
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 14:08:01 +1000

    Q4.2

--- movieArray.js
+++ movieArray.js
@@ -9,3 +9,19 @@ class Movie {
 }
 
 console.log(Movie);
+
+// Q4.2: Array of 10 movies
+let movieArray = [];
+
+movieArray.push(new Movie(2, "Worst Movie Ever", "2011", "8.3"));
+movieArray.push(new Movie(6, "They Were Out Of Zoos, So We Bought A Planetarium", "2019", "6.8"));
+movieArray.push(new Movie(4, "I Blame You For Everything", "2018", "9.0"));
+movieArray.push(new Movie(10, "Hostile Hospital", "2002", "0.0"));
+movieArray.push(new Movie(5, "We Bought Another Zoo", "2014", "3.2"));
+movieArray.push(new Movie(9, "The Woman from A.U.N.T.", "2016", "8.8"));
+movieArray.push(new Movie(7, "Lizards On A Bus", "2007", "1.1"));
+movieArray.push(new Movie(1, "Best Movie Ever", "2019", "4.5"));
+movieArray.push(new Movie(8, "(600) Days Of Summer", "2019", "4.5"));
+movieArray.push(new Movie(3, "The LEGO Github Movie", "2022", "7.9"));
+
+console.log(movieArray);
