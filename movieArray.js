4ce9ae4503fb7088bcba612b5e1594d58e0241cc
beiwa02 <bailiewalsh1@gmail.com>
2025-09-09 14:03:15 +1000

    Q4.1

---
+++ movieArray.js
@@ -0,0 +1,11 @@
+// Q4.1: Class definition
+class Movie {
+  constructor(movieID, movieTitle, movieYear, movieRating) {
+    this.movieID = movieID;
+    this.movieTitle = movieTitle;
+    this.movieYear = movieYear;
+    this.movieRating = movieRating;
+  }
+}
+
+console.log(Movie);
