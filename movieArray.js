9ec63c3e4478ecb68dc4aa6a451f77d061ce1a03
beiwa02 <bailiewalsh1@gmail.com>
2025-11-07 15:59:59 +1100

    Create Movie class

---
+++ movieArray.js
@@ -0,0 +1,9 @@
+// Movie class
+class Movie {
+  constructor(movieID, movieTitle, movieYear, movieRating) {
+    this.movieID = movieID;
+    this.movieTitle = movieTitle;
+    this.movieYear = movieYear;
+    this.movieRating = movieRating;
+  }
+}
